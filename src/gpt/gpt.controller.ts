c591fb377dde758cb6f3b1783e794ecf3d1ed10f
Frank Cairampoma <ingfralch@gmail.com>
2023-06-22 10:15:36 -0500

    funciona endpoint

--- src/gpt/gpt.controller.ts
+++ src/gpt/gpt.controller.ts
@@ -13,8 +13,52 @@ export class GptController {
   }
 
   @Get()
-  findAll() {
-    return this.gptService.findAll();
+  async findAll(@Body() data_JSON: any) {
+    const {pais} = data_JSON;
+    const {ingredientes} = data_JSON;
+    let ingredientes_todos = "";
+    for (let i = 0; i < ingredientes.length; i++) {
+        if (i === ingredientes.length - 1) {
+          ingredientes_todos += ingredientes[i];
+        } else {
+          ingredientes_todos += ingredientes[i] + ", ";
+        }
+    }
+    const respuesta = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + process.env.API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content:
+              `
+                            El usuario quiere cocinar algo con ${ingredientes_todos}  (no necesariamente se usan todos estos ingredientes) y 
+                            quiere cocinar una comida muy tipica de ${pais}, que no sea ningun tipo de tortilla a menos que el pais sea de mexico, puedes agregar algunos ingredientes,  ¿qué le recomiendas?, debes responder en español.
+                            Y en formato de JSON como por ejemplo:
+                            {
+                                "ingredientes": "...",
+                                "pais": "Colombia",
+                                "respuesta": "Arroz con pollo",
+                                "receta": "...",
+                                "informacion_nutricional": "..."
+                            }  
+                            SOLO BRINDAR UNA RESPUESTA EN FORMATO JSON, SIN COMENTARIOS NI NADA. 
+                        `
+          },
+          {
+            role: "user",
+            content: "..."
+          }
+        ]
+      })
+    })
+    const data = await respuesta.json();
+    return data.choices;
   }
 
   @Get(':id')
--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,51 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateGptDto } from './dto/create-gpt.dto';
 import { UpdateGptDto } from './dto/update-gpt.dto';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Injectable()
 export class GptService {
-  async create(createGptDto: CreateGptDto) {
 
+  async create(createGptDto: CreateGptDto) {
     return 'This action adds a new gpt';
   }
 
-  async findAll() {
-
-    const respuesta = await fetch("https://api.openai.com/v1/chat/completions", {
-      method: "POST",
-      headers: {
-        Authorization: "Bearer " ,
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        model: "gpt-3.5-turbo",
-        messages: [
-          {
-            role: "system",
-            content:
-              `
-                            El usuario quiere cocinar algo con arroz, papas, tomates (no necesariamente se usan todos estos ingredientes) y 
-                            quiere cocinar una comida muy tipica de peru, que no sea ningun tipo de tortilla a menos que el pais sea de mexico, puedes agregar algunos ingredientes,  ¿qué le recomiendas?, debes responder en español.
-                            Y en formato de JSON como por ejemplo:
-                            {
-                                "ingredientes": "...",
-                                "pais": "Colombia",
-                                "respuesta": "Arroz con pollo",
-                                "receta": "...",
-                                "informacion_nutricional": "..."
-                            }  
-                            SOLO BRINDAR UNA RESPUESTA EN FORMATO JSON, SIN COMENTARIOS NI NADA. 
-                        `
-          },
-          {
-            role: "user",
-            content: "..."
-          }
-        ]
-      })
-    })
-    const data = await respuesta.json();
-    return data.choices;
+   findAll() {
+    return `This action returns all gpt`;    
   }
 
   findOne(id: number) {
